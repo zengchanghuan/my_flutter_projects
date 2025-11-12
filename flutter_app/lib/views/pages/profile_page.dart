d82ef36aeb1b519032857e37ac49647999c11ab5
Tristan <tristan@qq.com>
2025-11-12 13:35:55 +0800

    SingleChildScrollView

--- flutter_app/lib/views/pages/profile_page.dart
+++ flutter_app/lib/views/pages/profile_page.dart
@@ -11,47 +11,65 @@ class _ProfilePageState extends State<ProfilePage> {
   final TextEditingController _controller = TextEditingController();
   bool _isChecked = false;
   bool _isSwitched = false;
+  double _sliderValue = 0.0;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextField(
-          controller: _controller,
-          decoration: const InputDecoration(
-            border: OutlineInputBorder(),
-          ),
-          onEditingComplete: () => setState(() {}),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+              onEditingComplete: () => setState(() {}),
+            ),
+            Text(_controller.text),
+            Checkbox.adaptive(
+                value: _isChecked,
+                onChanged: (bool? value) {
+                  setState(() {
+                    _isChecked = value!;
+                  });
+                }),
+            CheckboxListTile.adaptive(
+                title: const Text('click me'),
+                value: _isChecked,
+                onChanged: (bool? value) {
+                  setState(() {
+                    _isChecked = value!;
+                  });
+                }),
+            Switch.adaptive(
+                value: _isSwitched,
+                onChanged: (value) {
+                  setState(() {
+                    _isSwitched = value;
+                  });
+                }),
+            SwitchListTile.adaptive(
+                title: const Text('switch me'),
+                value: _isSwitched,
+                onChanged: (bool value) {
+                  setState(() {
+                    _isSwitched = value;
+                  });
+                }),
+            Slider(
+              max: 10.0,
+                value: _sliderValue,
+                onChanged: (double value) {
+                  setState(() {
+                    _sliderValue = value;
+                  });
+                }),
+
+          ],
         ),
-        Text(_controller.text),
-        Checkbox.adaptive(
-            value: _isChecked,
-            onChanged: (bool? value) {
-              setState(() {
-                _isChecked = value!;
-              });
-            }),
-        CheckboxListTile.adaptive(
-            title: const Text('click me'),
-            value: _isChecked,
-            onChanged: (bool? value) {
-              setState(() {
-                _isChecked = value!;
-              });
-            }),
-        Switch.adaptive(value: _isSwitched, onChanged: (value){
-          setState(() {
-            _isSwitched = value;
-          });
-        }),
-        SwitchListTile.adaptive(
-          title: const Text('switch me'),
-            value: _isSwitched, onChanged: (bool value){
-          setState(() {
-            _isSwitched = value;
-          });
-        })
-      ],
+      ),
     );
   }
 }
