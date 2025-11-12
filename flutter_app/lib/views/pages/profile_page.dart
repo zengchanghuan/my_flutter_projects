52b9d67815941a12c9fb785cb5bb14e233ca3eee
Tristan <tristan@qq.com>
2025-11-12 12:34:45 +0800

    add TextField

--- flutter_app/lib/views/pages/profile_page.dart
+++ flutter_app/lib/views/pages/profile_page.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('profile page'),
-    ) ;
+    return Column(
+      children: [
+        TextField(
+          controller: _controller,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+          ),
+          onEditingComplete: () {
+            setState(() {});
+          },
+        ),
+        Text(_controller.text),
+      ],
+    );
   }
 }
